68b6563698cd93fcb3e909e0621f7ce06c2bd6cd
darkhacker1906 <singhabhinandan634@gmail.com>
2025-03-01 11:16:04 +0530

    Added the caching to avoid multiple api calls and added the main page

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     
-<navbar/>
+<navbar v-if="route.path !== '/'"/>
     <NuxtPage />
   </div>
 </template>
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <style scoped>
 
--- stores/itemList.js
+++ stores/itemList.js
@@ -1,25 +1,49 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 import { useLoadingStore } from './loading';
 
 export const useItemList = defineStore('items', {
-  state: () => ({ 
-    items: []
-   }),
+  state: () => ({
+    items: JSON.parse(localStorage.getItem('cachedItems')) || [], 
+    fetchedAt: localStorage.getItem('fetchedAt') || null, 
+  }),
+
   actions: {
     async getPosts() {
       const loadingStore = useLoadingStore();
-      try{
+      if (this.isCacheValid()) {
+        return;
+      }
+
+      try {
         loadingStore.startLoading();
         const response = await $fetch('https://dummyjson.com/posts');
-        this.items=response.posts;
-      }catch(error){
-        console.error(error);
-      }finally{
+        this.items = response.posts;
+        this.fetchedAt = Date.now();
+
+        localStorage.setItem('cachedItems', JSON.stringify(this.items));
+        localStorage.setItem('fetchedAt', this.fetchedAt);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      } finally {
         loadingStore.stopLoading();
       }
     },
+
+
+    isCacheValid() {
+      if (!this.fetchedAt) return false;
+      const expirationTime = 15* 60 * 1000; 
+      const isExpired = Date.now() - this.fetchedAt > expirationTime;
+      if (isExpired) {
+        localStorage.removeItem('cachedItems');
+        localStorage.removeItem('fetchedAt');
+        return false;
+      }
+      return true;
+    },
   },
-})
+});
+
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useItemList, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useItemList, import.meta.hot));
 }
